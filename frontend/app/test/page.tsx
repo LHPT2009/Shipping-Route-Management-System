7a35bc6b5340bb6d2e63bf946920d70089a8f9a4
Tung <lehuynhphuongtung0601@gmail.com>
2024-07-29 00:29:41 +0700

    upgrade apollo

--- frontend/app/test/page.tsx
+++ frontend/app/test/page.tsx
@@ -3,22 +3,25 @@
 import type { RootState } from "../../lib/store/index";
 import { useAppDispatch, useAppSelector } from "../../lib/hooks/hooks";
 import { counterActions } from "../../lib/store/counter";
-import { fetchRoutes } from "@/query/demo";
-import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_ROUTES } from "@/query/route";
 
 interface RouteItem {
   id: number;
   name: string;
   user_id: number;
-  // add other properties as needed
 }
 
 export default function Home() {
-  const [listItem, setListItem] = useState<RouteItem[]>([]);
   const count = useAppSelector((state: RootState) => state.counter.value);
   const dispatch = useAppDispatch();
-  const datalist = fetchRoutes();
-  datalist.then((a) => setListItem(a));
+
+  const { loading, error, data } = useQuery(GET_ROUTES, {
+    pollInterval: 500,
+  });
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p>Error ...</p>;
+  const listItem = data.getRoutes;
   return (
     <main>
       <h1 className="text-6xl font-bold">Test page</h1>
@@ -33,11 +36,18 @@ export default function Home() {
       <p>____________________________</p>
       <ul>
         Load data:
-        {listItem.map((item, index) => (
-          <li key={index}>
-            {item.id}-{item.name}-{item.user_id}
+        {/* ex 1: */}
+        {listItem.map(({ id, name, user_id }: RouteItem) => (
+          <li key={id}>
+            {id}-{name}-{user_id}
           </li>
         ))}
+        {/* ex 2:
+        {listItem.map((item: any) => (
+          <li key={item.id}>
+            {item.id}-{item.name}-{item.user_id}
+          </li>
+        ))} */}
       </ul>
     </main>
   );
