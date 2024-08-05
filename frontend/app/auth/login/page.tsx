c1178fac13c71172e8e7ba24998f5b9e86dcbd3b
giahuy200202 <giahuy200202@gmail.com>
2024-08-05 16:17:30 +0700

    handle ui login

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -6,6 +6,7 @@ import { COLOR } from "@/constant";
 import Link from "next/link";
 import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import GoogleImg from "../../../public/images/login/google.png"
 import styles from "./login.module.scss";
 
 import * as yup from "yup";
@@ -36,8 +37,8 @@ const LoginPage = () => {
       <Form
         initialValues={{ remember: true }}
         style={{
-          width: "35%",
-          height: "40rem",
+          width: "35.5%",
+          height: "42rem",
           padding: "3rem",
           borderRadius: "1rem",
           backgroundColor: COLOR.BACKGROUNDBODY,
@@ -50,13 +51,14 @@ const LoginPage = () => {
           fontWeight: 700,
           color: COLOR.TEXT,
           marginBottom: 0
-        }}>Welcome back</Title>
+        }}>Welcome back !</Title>
 
         <Paragraph style={{
-          fontSize: "1.2rem",
+          fontSize: "1.1rem",
           marginTop: "0.9rem"
         }}>Enter your details to get sign in to your account</Paragraph>
 
+        {/* Email */}
         <Form.Item
           name="username"
           style={{ paddingBottom: errors.username ? "1rem" : 0, marginTop: "2.7rem" }}
@@ -80,6 +82,8 @@ const LoginPage = () => {
             )}
           />
         </Form.Item>
+
+        {/* Password */}
         <Form.Item
           name="password"
           style={{ paddingBottom: errors.password ? "1rem" : 0 }}
@@ -104,50 +108,54 @@ const LoginPage = () => {
             )}
           />
         </Form.Item>
-        <Flex justify="space-between" align="flex-start">
-          <Form.Item style={{display:"flex", alignItems: "flex-start"}}>
+
+        {/* Remember me */}
+        <Flex justify="space-between" align="flex-start" style={{ marginTop: 0, padding: 0 }}>
+          <Form.Item style={{ display: "flex", alignItems: "flex-start" }}>
             <Checkbox>Remember me</Checkbox>
           </Form.Item>
-          <Paragraph style={{
+          <Link href={"/auth/register"} style={{
             fontSize: "0.95rem",
             fontWeight: 600,
             color: COLOR.PRIMARY,
             marginTop: "0.3rem"
-          }}>Forgot password?</Paragraph>
+          }}>Forgot password?</Link>
         </Flex>
+
+        {/* Button login*/}
         <Form.Item>
           <Button
             type="primary"
             htmlType="submit"
             className="login-form-button"
-            style={{ width: "100%", borderRadius: "8px" }}
-            size="large"
+            style={{ width: "100%", borderRadius: "0.5rem", height: "2.8rem", marginTop: "0.5rem" }}
           >
             Log in
           </Button>
         </Form.Item>
-        <Form.Item
-          style={{
-            display: "flex",
-            justifyContent: "end",
-          }}
-        >
-          <Text>
-            Or <Link href={"/auth/register"}>register now!</Link>
-          </Text>
-        </Form.Item>
-        <Divider>
-          <Title level={4}>Other</Title>
+
+        <Divider style={{ borderColor: '#adb5bd', padding: "0 6rem", margin: 0 }} plain>
+          Or
         </Divider>
+
         <Form.Item>
           <Button
-            type="primary"
+            htmlType="submit"
             className="login-form-button"
-            style={{ width: "100%", borderRadius: "8px" }}
-            size="large"
+            style={{ width: "100%", borderRadius: "0.5rem", height: "2.8rem", marginTop: "1.2rem", background: "white" }}
           >
-            Login by Email
+            <Flex justify="center" align="center">
+              <img src={GoogleImg.src} alt="Google" style={{ width: "1.3rem", height: "1.3rem", marginRight: "0.8rem" }} />
+              Continue with Google
+            </Flex>
           </Button>
+        </Form.Item>
+
+        <Form.Item style={{ textAlign: "center", marginTop: "2.8rem" }}>
+          <Text style={{ fontSize: "0.95rem", color: "grey" }}>
+            Don't have an account? {" "}
+            <Link href={"/auth/register"} style={{ color: COLOR.PRIMARY, fontWeight: 500 }}>Register now</Link>
+          </Text>
         </Form.Item>
       </Form>
     </Flex>
