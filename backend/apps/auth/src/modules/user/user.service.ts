c6d53ea31d1045b7ba0fd93fa049012a2ee1978a
Tung <lehuynhphuongtung0601@gmail.com>
2024-10-02 19:48:28 +0700

    update unit test auth

--- backend/apps/auth/src/modules/user/user.service.ts
+++ backend/apps/auth/src/modules/user/user.service.ts
@@ -429,6 +429,8 @@ export class UserService {
         redisUserId => user.id.toString() === redisUserId
       );
 
+      console.log("userInRedisAffected", userInRedisAffected);
+
       if (userInRedisAffected) {
         const role: RoleEntity = await this.roleRepository.findOne({
           where: { id: userUpdateRoleDto.roles },
@@ -448,6 +450,7 @@ export class UserService {
           permissions: permissionNames,
         };
 
+        console.log("result", result);
         await this.cacheManager.set(userInRedisAffected, result);
       }
       return new ResponseDto(STATUS_CODE.CREATE, STATUS.CREATE, user, []);
