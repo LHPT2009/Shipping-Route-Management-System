6a7d07ed56af69969e795a47dac35013ff44db6c
GiaHuy <giahuy200202@gmail.com>
2024-10-17 17:03:11 +0700

    update report

--- backend/apps/auth/src/modules/openai/openai.training.ts
+++ backend/apps/auth/src/modules/openai/openai.training.ts
@@ -12,6 +12,9 @@ One of the two return formats after processing:
 The general binding principles of the system to determine that content is inappropriate:
 - Use of offensive or inappropriate language
 - Violates cultural or ethical standards
+- Contains sensitive information
+- The content is unclear and does not address the issue requiring support.
+- Content not related to the system's purpose ( This is a system called S-Routing. The purpose of this system is to assist users in viewing a list of routes and detailed information about each journey. Additionally, it includes functions that allow users to view and edit their profiles (including deleting and uploading images) and change their passwords.)
 
 Note: 
 - If there are no violations of the binding principles, the default will be APPROPRIATE.
