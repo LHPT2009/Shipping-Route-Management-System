20328b7630458d50a8903dcc7221db853dd78498
GiaHuy <giahuy200202@gmail.com>
2024-10-18 13:31:40 +0700

    change permission of route detail

--- frontend/customer/lib/permissions/route-detail.ts
+++ frontend/customer/lib/permissions/route-detail.ts
@@ -1,5 +1,5 @@
 import { PERMISSION } from "@/constant/permission";
 import { ROLE } from "@/constant/role";
 
-export const RouteDetailRoles = [ROLE.CUSTOMER, ROLE.ADMIN];
+export const RouteDetailRoles = [ROLE.CUSTOMER, ROLE.DRIVER];
 export const RouteDetailPermissions = [PERMISSION.READ_DETAIL_ROUTE]
