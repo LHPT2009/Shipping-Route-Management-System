41261d7cea9b92dcfd5c68440e6cc5c4c2b05379
GiaHuy <giahuy200202@gmail.com>
2024-10-19 15:57:53 +0700

    clear responsive redundance

--- frontend/admin/app/unauthorized/page.tsx
+++ frontend/admin/app/unauthorized/page.tsx
@@ -4,13 +4,10 @@ import Paragraph from "antd/es/typography/Paragraph";
 import Unauthorized from "@/public/images/unauthorized/unauthorized.png";
 import { Button, Flex } from "antd";
 import { useRouter } from "next/navigation";
-import { GetValueFromScreen, UseScreenWidth } from "@/utils/screenUtils";
 
 const UnauthorizedPage = () => {
   const router = useRouter();
-  const screenWidth = UseScreenWidth();
-  const responsive = GetValueFromScreen(screenWidth, true, true, true, true);
-  
+
   return (
     <Flex align="center" justify="center" vertical style={{ margin: "6rem auto", textAlign: "center" }}>
       <img src={Unauthorized.src} style={{ width: "20rem" }} />
@@ -20,16 +17,11 @@ const UnauthorizedPage = () => {
       <Button
         onClick={() => router.push("/")}
         type="primary"
-        style={responsive ? {
+        style={{
           margin: "2rem",
           padding: "1.5rem",
           border: "none",
-        } : {
-          margin: "2rem",
-          padding: "1.5rem",
-          border: "none",
-        }
-        }
+        }}
       >
         Go to homepage
       </Button>
